2f001363e54724943b001b56f208d73ead3f785f
TaurusXin <zyx457601@gmail.com>
2021-07-09 21:47:18 +0800

    refactor: empty default project template structure

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Media Converter</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+  }
+})
+</script>
